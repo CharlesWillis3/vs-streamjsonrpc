0e6b4aa9c8ffee13394a5abe720a2b21c9d5228e
Charles Willis <chawill@microsoft.com>
2024-12-21 22:59:24 +0000

    Add unit tests

---
+++ test/StreamJsonRpc.Tests/AsyncEnumerableNerdbankMessagePackTests.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+public class AsyncEnumerableNerdbankMessagePackTests : AsyncEnumerableTests
+{
+    public AsyncEnumerableNerdbankMessagePackTests(ITestOutputHelper logger)
+        : base(logger)
+    {
+    }
+
+    protected override void InitializeFormattersAndHandlers()
+    {
+        this.serverMessageFormatter = new NerdbankMessagePackFormatter();
+        this.clientMessageFormatter = new NerdbankMessagePackFormatter();
+    }
+}
---
+++ test/StreamJsonRpc.Tests/DuplexPipeMarshalingNerdbankMessagePackTests.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+public class DuplexPipeMarshalingNerdbankMessagePackTests : DuplexPipeMarshalingTests
+{
+    public DuplexPipeMarshalingNerdbankMessagePackTests(ITestOutputHelper logger)
+        : base(logger)
+    {
+    }
+
+    protected override void InitializeFormattersAndHandlers()
+    {
+        this.serverMessageFormatter = new NerdbankMessagePackFormatter { MultiplexingStream = this.serverMx };
+        this.clientMessageFormatter = new NerdbankMessagePackFormatter { MultiplexingStream = this.clientMx };
+    }
+}
